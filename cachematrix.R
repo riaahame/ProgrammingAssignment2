27cf738dea4b8277e67bbde30a763236d4d4ec45
Riaz Ahamed <riaahame@cisco.com>
2015-11-22 11:43:54 -0500

    Updated version with more comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R program contains definition and usage of two functions "makeCacheMatrix" and "cacheSolve" 
+## Function makeCacheMatrix can be used to define a special matrix object that
+##          can cache its inverse. It uses function setInverse to set the cache and 
+##          getInverse to retrieve the cache value.
+## Function cacheSolve is used to get the inverse of the matrix. It returns the value
+##          from the cache if available. If not, it would calculate the inverse and 
+##          store in cache and return the inverse.
+         
 
-## Write a short comment describing this function
-
+## This function creates a "matrix" object that can cache its inverse 
+## It defines couple of other functions such as setInverse & getInverse
+## setInverse function is used to set the value to the cache
+## getInverse is used to get the cache value
 makeCacheMatrix <- function(x = matrix()) {
 
         Inv <- NULL
@@ -13,20 +21,27 @@ makeCacheMatrix <- function(x = matrix()) {
         get <- function () x
         setInverse <- function(inverse) Inv <<- inverse
         getInverse <- function() Inv 
-        list (set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+        list (set = set, get = get, setInverse = setInverse,
+              getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function gets the inverse of the matrix from the cache if its available in cache.
+## If not, the inverse is calculated using the "solve" function and the cache is updated.  
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         
+        #First check in cache
         Inv <- x$getInverse()
         if (!is.null(Inv)) {
+                #Inverse available in cache
                 print("Getting cached data for Inverse...")
                 return(Inv)
         }
+        
+        #Inverse not available in cache
+        #Calculate inverse using "solve" function
+        
         data <-x$get()
         Inv <- solve(data, ...)
         x$setInverse(Inv)
